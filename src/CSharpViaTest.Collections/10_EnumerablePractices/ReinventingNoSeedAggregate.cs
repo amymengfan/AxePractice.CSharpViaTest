361f1b9ab93931d43ab7b8c9c11968d84a7fed07
mengfan <mffeng@thoughtworks.com>
2017-06-15 08:35:13 +0800

    [mengfan] finish ReinventingNoSeedAggregate

--- src/CSharpViaTest.Collections/10_EnumerablePractices/ReinventingNoSeedAggregate.cs
+++ src/CSharpViaTest.Collections/10_EnumerablePractices/ReinventingNoSeedAggregate.cs
@@ -37,7 +37,19 @@ namespace CSharpViaTest.Collections._10_EnumerablePractices
             this IEnumerable<TSource> source,
             Func<TSource, TSource, TSource> func)
         {
-            throw new NotImplementedException();
+            if (source == null) throw new ArgumentNullException();
+            if (func == null) throw new ArgumentNullException();
+
+            using(IEnumerator<TSource> enumerator = source.GetEnumerator()) {
+                if (!enumerator.MoveNext()) throw new InvalidOperationException();
+
+                TSource seed = enumerator.Current;
+                while (enumerator.MoveNext()) {
+                    TSource current = enumerator.Current;
+                    seed = func(seed, current);
+                }
+                return seed;
+            }
         }
 
         #endregion
